1c1350a636e75e1a4eb80c4fe4449a682a57414c
jasonzhou903 <jasonzhou903@qq.com>
2017-08-09 11:09:15 +0800

    fix test with Route or Link

--- src/__tests__/components/Home.js
+++ src/__tests__/components/Home.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
+import { MemoryRouter } from 'react-router-dom'
 
 import Home from '../../components/Home'
 
 const setup = (user = {}) => {
   const component = <Home user={user} />
+  const componentWithRouter = (
+    <MemoryRouter>
+      <Home user={user} />
+    </MemoryRouter>
+  )
   const wrapper = shallow(component)
 
   return {
     component,
+    componentWithRouter,
     wrapper,
   }
 }
@@ -28,8 +35,8 @@ describe('Home component', () => {
   })
 
   it("should render right thing and don't change unexpected", () => {
-    const { component } = setup()
-    const tree = renderer.create(component).toJSON()
+    const { componentWithRouter } = setup()
+    const tree = renderer.create(componentWithRouter).toJSON()
 
     expect(tree).toMatchSnapshot()
   })
